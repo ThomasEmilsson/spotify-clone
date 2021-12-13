a9d0155e2aa34348912c1b076f53676686562bee
Thomas Emilsson <thomasemilsson10@gmail.com>
2021-12-13 20:44:58 +0100

    Update Sidebar: Add musicMenu and playlists

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -37,10 +37,25 @@ const navMenu = [
   },
 ];
 
+const musicMenu = [
+  {
+    name: "Create Playlist",
+    icon: MdPlaylistAdd,
+    route: "/",
+  },
+  {
+    name: "Favorites",
+    icon: MdFavorite,
+    route: "/favorites",
+  },
+];
+
+const playlists = new Array(30).fill(1).map((_, i) => `Playlist ${i + 1}`);
+
 const Sidebar = () => {
   return (
     <Box
-      width="100%"
+      //   width="100%"
       height="calc(100vh - 100px)"
       paddingX="5px"
       color="gray"
@@ -70,6 +85,40 @@ const Sidebar = () => {
             ))}
           </List>
         </Box>
+        <Box marginBottom="20px">
+          <List spacing={2}>
+            {musicMenu.map((menu) => (
+              <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
+                <LinkBox>
+                  <NextLink href={menu.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon
+                        as={menu.icon}
+                        color="white"
+                        marginRight="20px"
+                      />
+                      {menu.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Box>
+      <Divider color="gray.800" />
+      <Box height="66%" overflowY="auto" paddingY="20px">
+        <List spacing={2}>
+          {playlists.map((playlist) => (
+            <ListItem paddingX="20px" key={playlist}>
+              <LinkBox>
+                <NextLink href="/" passHref>
+                  <LinkOverlay>{playlist}</LinkOverlay>
+                </NextLink>
+              </LinkBox>
+            </ListItem>
+          ))}
+        </List>
       </Box>
     </Box>
   );
