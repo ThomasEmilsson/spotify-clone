57fdb9d505c5e2c4f17477f50995d7c146d3d5f0
Thomas Emilsson <thomasemilsson10@gmail.com>
2021-12-16 19:50:29 +0100

    Add Gradient Layout: Profile, title, image, subtitle, description

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,17 @@
+import GradientLayout from "../components/gradientLayout";
+
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <GradientLayout
+      color="green"
+      title="13 Characters"
+      subtitle="profile"
+      description="10 public playlists"
+      image="./profile.png"
+    >
+      <div> home page </div>
+    </GradientLayout>
+  );
 };
 
 export default Home;
