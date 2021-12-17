e541632204016aafac350403e6c4bc3296566849
Thomas Emilsson <thomasemilsson10@gmail.com>
2021-12-17 21:56:28 +0100

    Add player icons to player

--- components/player.tsx
+++ components/player.tsx
@@ -1,6 +1,6 @@
 import {
-  ButtonGroup,
   Box,
+  ButtonGroup,
   IconButton,
   RangeSlider,
   RangeSliderFilledTrack,
@@ -8,9 +8,9 @@ import {
   RangeSliderThumb,
   Center,
   Text,
-} from "@chakra-ui/layout";
+} from "@chakra-ui/react";
 import ReactHowler from "react-howler";
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   MdShuffle,
   MdSkipPrevious,
@@ -22,7 +22,57 @@ import {
 import { useStoreActions } from "easy-peasy";
 
 const Player = () => {
-  return <Box> hello from player</Box>;
+  return (
+    <Box>
+      <Box>{/* <ReactHowler /> */}</Box>
+      <Center color="gray.600">
+        <ButtonGroup>
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="shuffle"
+            fontSize="24px"
+            icon={<MdShuffle />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="skip"
+            fontSize="24px"
+            icon={<MdSkipPrevious />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="play"
+            fontSize="40px"
+            icon={<MdOutlinePlayCircleFilled />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="pause"
+            fontSize="40px"
+            icon={<MdOutlinePauseCircleFilled />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="next"
+            fontSize="24px"
+            icon={<MdSkipNext />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="repeat"
+            fontSize="24px"
+            icon={<MdOutlineRepeat />}
+          />
+        </ButtonGroup>
+      </Center>
+    </Box>
+  );
 };
 
 export default Player;
